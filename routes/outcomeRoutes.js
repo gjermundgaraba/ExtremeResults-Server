964b6cbf500daa598b0e21b04755fc6f8cb52fa2
Gjermund Bjaanes <bjaanes@gmail.com>
2016-02-16 20:26:43 +0100

    Added GET, PUT and PATCH to outcomes

--- routes/outcomeRoutes.js
+++ routes/outcomeRoutes.js
@@ -15,8 +15,70 @@ var routes = function (Outcome) {
         })
         .post(function (req, res) {
             var outcome = new Outcome(req.body);
-            outcome.save();
-            res.status(201).send(outcome);
+            outcome.save(function (err) {
+                if (err) {
+                    res.status(500).send(error);
+                } else {
+                    res.status(201).send(outcome);
+                }
+            });
+        });
+
+    outcomeRouter.use('/:outcomeId', function (req, res, next) {
+        Outcome.findById(req.params.outcomeId, function (err, outcome) {
+            if (err) {
+                res.status(500).send(err);
+            } else if (outcome) {
+                req.outcome = outcome;
+                next();
+            } else {
+                res.status(404).send('No outcome with that id found');
+            }
+        });
+    });
+    outcomeRouter.route('/:outcomeId')
+        .get(function (req, res) {
+            res.json(req.outcome);
+        })
+        .put(function (req, res) {
+            req.outcome.typeName = req.body.typeName;
+            req.outcome.firstStory = req.body.firstStory;
+            req.outcome.secondStory = req.body.secondStory;
+            req.outcome.thirdStory = req.body.thirdStory;
+            req.outcome.effectiveDate = req.body.effectiveDate;
+            req.outcome.save(function (error) {
+                if (error) {
+                    res.status(500).send(error);
+                } else {
+                    res.json(req.outcome);
+                }
+            });
+        })
+        .patch(function (req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+
+            for (var outcomeField in req.body) {
+                req.outcome[outcomeField] = req.body[outcomeField];
+            }
+
+            req.outcome.save(function (err) {
+                if (err) {
+                    res.status(500).send(error);
+                } else {
+                    res.json(req.outcome);
+                }
+            });
+        })
+        .delete(function (req, res) {
+            req.outcome.remove(function (error) {
+                if (error) {
+                    res.status(500).send(error);
+                } else {
+                    res.status(204).send();
+                }
+            });
         });
 
     return outcomeRouter;
